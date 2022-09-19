cf2a25e57913a4f88b65dbaea56b4c3658c3a52f
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-19 14:56:15 +0300

    Add class

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-$n = rand(1, 6);
+$n = rand(1, 12);
 switch ($n){
     case 1: echo "Класс $n: Учим буквы"; break;
     case 2: echo "Класс $n: Учим таблицу умножения"; break;
     case 3: echo "Класс $n: делаем упражнения"; break;
     case 4: echo "Класс $n: учим английский"; break;
     case 5: echo "Класс $n: ходим на физ-ру"; break;
+    case 6: echo "Класс $n: учим биологию"; break;
+    case 7: echo "Класс $n: учим алгебру"; break;
+    case 8: echo "Класс $n: учим историю"; break;
+    case 9: echo "Класс $n: готовимся к пда"; break;
+    case 10: echo "Класс $n: кайфуем"; break;
+    case 11: echo "Класс $n: готовимся к зно"; break;
     default: echo "<span style=\"color:green;\">Такого класса у нас нет!</span>";
 }
